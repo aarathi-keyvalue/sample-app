b549f956d12641875a857b15e80d918e33e64a55
fathima-mirza <fathima@keyvalue.systems>
2022-09-02 17:49:26 +0530

    added cart listing page and its functionalities

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import React from 'react'
+import { useContext } from 'react';
+import { Store } from 'utils/Store';
 
 export default function Layout({title, children}) {
+    const {state,dispatch}=useContext(Store);
+    const {cart}=state;
   return (
    <>
    <Head>
@@ -19,7 +23,14 @@ export default function Layout({title, children}) {
             </Link>
             <div>
                 <Link href="/cart">
-                    <a className="p-2">cart</a>
+                    <a className="p-2">
+                        cart
+                        {cart.cartItems.length > 0 && (
+                            <span className='ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white'>
+                                {cart.cartItems.reduce((a,c)=>a+c.quantity, 0)}
+                            </span>
+                        )}
+                    </a>
                     </Link>
                 <Link href="/login">
                 <a className="p-2">login</a>
--- src/components/pricecard/index.tsx
+++ src/components/pricecard/index.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Router } from '@i18n';
+import { useRouter } from 'next/router';
+import React, { useContext } from 'react'
 import { Button } from '..';
+import {Store} from "../../../utils/Store"
 
 export default function PriceCard({product}) {
-    console.log("In Card:", product);
+
+  const { state,dispatch} = useContext(Store);
+
+  const router=useRouter();
+
+    const addToCartHandler = () => {
+      const existItem = state.cart.cartItems.find((x)=> x.slug === product.slug);
+      const quantity = existItem ? existItem.quantity + 1 : 1;
+
+      if(product.countInStock < quantity){
+        alert('Sorry. Product is out of Stock');
+        return;
+      }
+        dispatch({type:'CART_ADD_ITEM', payload: {...product, quantity}});
+        router.push('/cart');
+    };
+
   return (
     <div className='flex flex-col justify-between px-6 py-5 border-solid border-2  space-y-2 rounded-lg shadow-md'>
         <div className='flex justify-between'>
@@ -13,7 +32,7 @@ export default function PriceCard({product}) {
             <p>Status</p>
             <p>Unavailable</p>
         </div>
-        <Button> Add to cart </Button>
+        <Button onClick={addToCartHandler}> Add to cart </Button>
     </div>
   )
 }
