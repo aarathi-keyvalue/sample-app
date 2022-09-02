9386f8751a03972a5636630cd141e964bbf797a2
fathima-mirza <fathima@keyvalue.systems>
2022-09-02 11:04:35 +0530

    added product listing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-
+import Layout from "../src/components/Layout.js"
+import ProductItem from "../src/components/ProductItem"
+import data from "../utils/data"
 import { Container, Header, Main, Footer, Cards } from "@components";
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Header />
+      {/* <Header />
       <Main />
       <Cards />
-      <Footer />
+      <Footer /> */}
+      <Layout title="HomePage"> 
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {data.products.map((product)=>(
+            <ProductItem product={product} key={product.slug}></ProductItem>
+          ))}
+
+        </div>
+       </Layout>
     </Container>
   );
 };
