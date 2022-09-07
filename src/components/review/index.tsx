cd3344fee2cf734bc2d0d5486518960b1bae5fe6
aarathi00 <aarathi.b@keyvalue.systems>
2022-09-07 13:58:47 +0530

    added place-order

--- src/components/review/index.tsx
+++ src/components/review/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function ReviewCard() {
   return (
     <div className='border-solid border-2 rounded-lg shadow-md px-7 py-7 my-9'>
-        <p>Please <Link href={'/login'}> login </Link> to write a review</p>
+        <p>Please <Link href='/login'><a className='text-blue-600'>login</a></Link> to write a review</p>
     </div>
   )
 }
