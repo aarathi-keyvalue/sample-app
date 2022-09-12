246245bcd86736f8a4d5a7461bc5d8ceec06a526
fathima-mirza <fathima@keyvalue.systems>
2022-09-12 14:27:05 +0530

    added carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Layout from "../src/components/Layout"
-import ProductItem from "../src/components/ProductItem"
-import data from "../utils/data"
+import Layout from "../src/components/Layout";
+import ProductItem from "../src/components/ProductItem";
+import data from "../utils/data";
 import { Container, Header, Main, Footer, Cards } from "@components";
+import SliderComponent from "../src/components/SliderComponent";
 
 const Home: React.FC = () => {
   return (
@@ -11,14 +12,18 @@ const Home: React.FC = () => {
       <Main />
       <Cards />
       <Footer /> */}
-      <Layout title="HomePage"> 
+      <Layout title="HomePage">
+        <div className="my-5">
+          <SliderComponent></SliderComponent>
+        </div>
+ <h1 className="text-2xl my-3">Featured Products</h1>
+       
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-          {data.products.map((product)=>(
+          {data.products.map((product) => (
             <ProductItem product={product} key={product.slug}></ProductItem>
           ))}
-
         </div>
-       </Layout>
+      </Layout>
     </Container>
   );
 };
---
+++ src/components/SliderComponent.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default function SliderComponent() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            className="object-fill w-full rounded-lg relative"
+            src="/images/banner1.jpg"
+            alt="image slide 1"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="object-fill w-full rounded-lg relative"
+            src="/images/banner2.jpg"
+            alt="image slide 2"
+          />
+        </SwiperSlide>
+    
+      </Swiper>
+    </>
+  );
+}
