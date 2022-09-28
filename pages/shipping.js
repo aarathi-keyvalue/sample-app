5e618797eb2b1c9859a57bfaef6e81b8cfefea23
aarathi00 <aarathi.b@keyvalue.systems>
2022-09-28 12:32:06 +0530

    integrated shipping module

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,8 +1,28 @@
 import Layout from "../src/components/Layout";
 import { Button } from "../src/components";
 import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 export default function shipping() {
+  const [state, setState] = useState({
+    name: "",
+    mobileNumber: "",
+    address: "",
+    city: "",
+    country: "",
+    pincode: "",
+  });
+
+  const onChangeValue = (key, value) => {
+    setState({
+      ...state,
+      [key]: value,
+    });
+  };
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
   return (
     <Layout title="Shipping">
       <form className="max-auto max-w-screen-md">
@@ -14,6 +34,7 @@ export default function shipping() {
             id="name"
             autoFocus
             type="text"
+            onChange={(event) => onChangeValue("name", event.target.value)}
           ></input>
         </div>
         <div className="mb-4 flex flex-col">
@@ -23,6 +44,7 @@ export default function shipping() {
             id="address"
             autoFocus
             type="text"
+            onChange={(event) => onChangeValue("address", event.target.value)}
           ></input>
         </div>
         <div className="mb-4 flex flex-col">
@@ -32,6 +54,7 @@ export default function shipping() {
             id="city"
             autoFocus
             type="text"
+            onChange={(event) => onChangeValue("city", event.target.value)}
           ></input>
         </div>
         <div className="mb-4 flex flex-col">
@@ -41,6 +64,7 @@ export default function shipping() {
             id="postcode"
             autoFocus
             type="text"
+            onChange={(event) => onChangeValue("pincode", event.target.value)}
           ></input>
         </div>
         <div className="mb-4 flex flex-col">
@@ -50,10 +74,30 @@ export default function shipping() {
             id="country"
             autoFocus
             type="text"
+            onChange={(event) => onChangeValue("country", event.target.value)}
+          ></input>
+        </div>
+        <div className="mb-4 flex flex-col">
+          <label htmlFor="mobileno"> Mobile Number </label>
+          <input
+            className=" appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="mobileno"
+            autoFocus
+            type="text"
+            onChange={(event) =>
+              onChangeValue("mobileNumber", event.target.value)
+            }
           ></input>
         </div>
         <Button className="bg-yellow-400 hover:bg-yellow-600">
-          <Link href={"/place-order"}>Next</Link>
+          <Link
+            href={{
+              pathname: "/place-order",
+              query: state,
+            }}
+          >
+            Next
+          </Link>
         </Button>
       </form>
     </Layout>
